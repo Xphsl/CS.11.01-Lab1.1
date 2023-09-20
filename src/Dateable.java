11b5bdf8c68810f0c292fe5819e14daea70deced
Xphsl <stevenl2026@student.cis.edu.hk>
2023-09-20 11:13:23 +0800

    Lab 1.1

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,16 @@
-public class Dateable {
+import java.util.Scanner;
+
+class Dateable {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+
+        int age = scanner.nextInt();
+
+        int minimumDatingAge = 7 + (age / 2);
+
+        System.out.println(age + "-year olds should date somebody who is at least " + minimumDatingAge + " years old.");
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter your age: ");
+
+        int age = scanner.nextInt();
+
+        int minimumnumberofpages = 100-age;
+
+        System.out.print(age +"-year olds should read at least "+ minimumnumberofpages +" pages before giving up on a book.");
     }
 }
